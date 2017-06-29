1c03bb0e16a773a8e6e97c130a1dab995429e2b8
jeffgladstone <jeff.gladstone2@gmail.com>
2017-06-29 12:07:59 -0400

    Version 1.0
    Site functionality. Improved mechanics and display.

--- checklist/views.py
+++ checklist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 import datetime
 from tasks.models import Task
+import re,urllib
 
 def sort_tasks_recent(tasks_data):
     '''sorts list of tasks in order by date created'''
@@ -12,6 +13,20 @@ def sort_tasks_recent(tasks_data):
 def homepage(request):
     now = datetime.datetime.now()
     tasks = sort_tasks_recent(Task.objects.all())
+    for task in tasks:
+        if ('task'+ str(task.id)) in request.GET:
+            q = request.GET['task'+ str(task.id)]
+            if (q == 'Complete'):
+                task.is_done = True
+                task.save()
+                return redirect('/', {'current_date': now, 'tasks': tasks})
+            elif (q == 'Incomplete'):
+                task.is_done = False
+                task.save()
+                return redirect('/', {'current_date': now, 'tasks': tasks})
+            elif (q == 'Delete'):
+                task.delete()
+                return redirect('/', {'current_date': now, 'tasks': tasks})
     return render(request, 'homepage.html', {'current_date': now, 'tasks': tasks})
 
 def add_task(request):
--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
     description = models.CharField(max_length=100)
     date_created = models.DateTimeField()
-    is_done = models.BooleanField(default = True)
+    is_done = models.BooleanField(default = False)
 
     def __str__(self):
         return self.description
